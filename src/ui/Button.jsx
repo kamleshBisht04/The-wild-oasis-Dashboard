e68b3cf78c69123e0500a5072b299b4abfdd1b1e
kamleshBisht04 <kamleshbisht04@gmail.com>
2025-12-28 22:50:21 +0530

    add all

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,7 @@
 function Button({ children, variation = "default" }) {
   const base = "rounded-sm px-1 py-1 text-xs font-medium transition";
-
+    //  button class for all
+    
   const styles = {
     default: "text-gray-600 hover:bg-emerald-600 hover:text-white",
     active: "bg-emerald-600 text-white hover:bg-emerald-600",
